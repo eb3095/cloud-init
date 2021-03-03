1bab690f6a7a2abad5ac85ed09be8f1c6d10ae33
ebenner <ebennerit@gmail.com>
2021-03-03 17:00:12 -0500

    Debugging

--- cloudinit/sources/helpers/vultr.py
+++ cloudinit/sources/helpers/vultr.py
@@ -19,7 +19,7 @@ from functools import lru_cache
 LOG = log.getLogger(__name__)
 
 
-@lru_cache(maxsize=None)
+@lru_cache()
 def get_metadata(params):
     # Make sure interface is not up already
     if net.has_url_connectivity(params['url']):
